f274e8324fda526976da14a062e68db57e672efe
Gjermund Bjaanes <bjaanes@gmail.com>
2015-07-13 21:12:11 +0200

    Added getActiveEntries function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+var moment = require('moment');
 
 /*
  * Function to get all Agile Results type entries (Daily Outcomes, Monday Visions, Weekly Reflections, etc...)
@@ -8,23 +8,16 @@
 Parse.Cloud.define("getEntries", function(request, response) {
     var entries = [];
 
-    var dailyOutcomesQuery = new Parse.Query("DailyOutcome");
-    var mondayVisionQuery = new Parse.Query("MondayVision");
-    var weeklyReflectionsQuery = new Parse.Query("WeeklyReflection");
+    var outcomesQuery = new Parse.Query("Outcome");
+    var reflectionsQuery = new Parse.Query("Reflection");
 
-    dailyOutcomesQuery.find()
-        .then(function(dailyOutcomes) {
-            entries.push.apply(entries, dailyOutcomes);
+    outcomesQuery.find()
+        .then(function(outcomes) {
+            entries.push.apply(entries, outcomes);
 
-            return mondayVisionQuery.find();
-        })
-        .then(function(mondayVisions) {
-            entries.push.apply(entries, mondayVisions);
-
-            return weeklyReflectionsQuery.find();
-        })
-        .then(function(weeklyReflections) {
-            entries.push.apply(entries, weeklyReflections);
+            return reflectionsQuery.find();        })
+        .then(function(reflections) {
+            entries.push.apply(entries, reflections);
 
             entries.sort(entriesSortFunction);
             response.success(entries);
@@ -47,4 +40,61 @@ Parse.Cloud.define("getEntries", function(request, response) {
 
 });
 
+/*
+ * Function to get all current active outcomes
+ *
+ * Eg: Today's outcomes, this weeks outcomes etc.
+ *
+ * Returns an array with current active outcomes
+ */
+Parse.Cloud.define("getActiveEntries", function(request, response) {
+    var entries = [];
+
+    // Daily
+    var dailyOutcomeQuery = new Parse.Query("Outcome");
+    dailyOutcomeQuery.equalTo("typeName", "Daily");
+
+
+    var startDateForDaily = moment().startOf("day").toDate();
+    console.log("Start Date For Daily: " + startDateForDaily);
+    dailyOutcomeQuery.greaterThanOrEqualTo("createdAt", startDateForDaily);
+
+    var finishDateForDaily = moment().endOf("day").toDate();
+    console.log("Finish Date For Daily: " + finishDateForDaily);
+    dailyOutcomeQuery.lessThan("createdAt", finishDateForDaily);
+
+
+    // Weekly
+    var weeklyOutcomeQuery = new Parse.Query("Outcome");
+    weeklyOutcomeQuery.equalTo("typeName", "Weekly");
+
+    var startOfWeek = moment().startOf("week").startOf("day").toDate();
+    console.log("Start Of The Week: " + startOfWeek);
+    weeklyOutcomeQuery.greaterThanOrEqualTo("createdAt", startOfWeek);
+
+    var endOfWeek = moment().endOf("week").endOf("day").toDate();
+    console.log("End Of The Week: " + endOfWeek);
+    weeklyOutcomeQuery.lessThan("createdAt", endOfWeek);
+
+    dailyOutcomeQuery.first()
+        .then(function (dailyOutcome) {
+            console.log("Result for Daily Outcome: " + dailyOutcome);
+            if (dailyOutcome !== undefined) {
+                entries.push(dailyOutcome);
+            }
+
+            return weeklyOutcomeQuery.first();
+        })
+        .then(function (weeklyOutcome) {
+            console.log("Result for Weekly Outcome: " + weeklyOutcome);
+            if (weeklyOutcome !== undefined) {
+                entries.push(weeklyOutcome);
+            }
+
+            response.success(entries);
+        }, function (error) {
+            response.error(error);
+        });
+
+});
 
